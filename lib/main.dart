3e29958331b136baed05b92e185d3b7a8bad5ecb
Jinosh05 <jinoshprabhuraj5@gmail.com>
2023-08-15 14:46:47 +0530

    Implemented Bloc in UI part

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:dictionary_api/models/dictionary_model.dart';
-import 'package:dictionary_api/provider/api_provider.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'bloc/dictionary_bloc.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,8 +13,8 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (context) => ApiProvider(),
+    return BlocProvider(
+      create: (context) => DictionaryBloc(),
       child: const MaterialApp(
         home: HomeScreen(),
       ),
@@ -32,94 +33,99 @@ class _HomeScreenState extends State<HomeScreen> {
   final TextEditingController controller = TextEditingController();
 
   @override
-  void initState() {
-    super.initState();
-    WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
-      Provider.of<ApiProvider>(context, listen: false).getValues('');
-    });
-  }
-
-  @override
   Widget build(BuildContext context) {
     double width = MediaQuery.of(context).size.width;
+    DictionaryBloc bloc = BlocProvider.of(context);
+    bloc.add(const SearchWord(" "));
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Dictionary API'),
       ),
-      body: Consumer<ApiProvider>(
-        builder: (context, ApiProvider value, child) {
-          return Column(
-            children: [
-              Container(
-                margin: const EdgeInsets.symmetric(vertical: 10),
-                width: width * 0.9,
-                child: Expanded(
-                  child: TextField(
-                    controller: controller,
-                    decoration: InputDecoration(
-                        border: const OutlineInputBorder(),
-                        suffixIcon: value.isLoading
-                            ? const Padding(
-                                padding: EdgeInsets.all(12.0),
-                                child: CircularProgressIndicator(),
-                              )
-                            : IconButton(
-                                onPressed: () {
-                                  controller.clear();
-                                  value.getValues(' ');
-                                },
-                                icon: const Icon(
-                                  Icons.clear,
-                                  size: 25,
-                                ))),
-                    onChanged: ((text) {
-                      value.getValues(controller.text);
-                    }),
+      body: Column(
+        children: [
+          Container(
+            margin: EdgeInsets.symmetric(vertical: 10, horizontal: width * 0.5),
+            child: Expanded(
+              child: TextField(
+                controller: controller,
+                decoration: InputDecoration(
+                  border: const OutlineInputBorder(),
+                  suffixIcon: BlocBuilder<DictionaryBloc, DictionaryState>(
+                    builder: (context, state) {
+                      return state is DictionaryLoading
+                          ? const Padding(
+                              padding: EdgeInsets.all(12.0),
+                              child: CircularProgressIndicator(),
+                            )
+                          : IconButton(
+                              onPressed: () {
+                                controller.clear();
+                                bloc.add(const SearchWord(" "));
+                              },
+                              icon: const Icon(
+                                Icons.clear,
+                                size: 25,
+                              ),
+                            );
+                    },
                   ),
                 ),
+                onChanged: ((text) {
+                  bloc.add(SearchWord(text));
+                }),
               ),
-              Expanded(
-                child: ListView.separated(
-                  itemCount: value.dictValue == null
-                      ? 0
-                      : value.dictValue!.list.length,
-                  itemBuilder: (BuildContext context, int index) {
-                    List<ListElement> ld = value.dictValue!.list;
-                    return Container(
-                      color: index.isEven ? Colors.black12 : Colors.white,
-                      padding: const EdgeInsets.all(10),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          SizedBox(
-                              width: MediaQuery.of(context).size.width * 0.20,
+            ),
+          ),
+          Expanded(
+            child: BlocBuilder<DictionaryBloc, DictionaryState>(
+              builder: (context, state) {
+                if (state is DictionaryLoading) {
+                  return const Center(
+                    child: CircularProgressIndicator(),
+                  );
+                } else if (state is DictionaryLoaded) {
+                  return ListView.separated(
+                    itemCount: state.dictValue.list.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      List<ListElement> ld = state.dictValue.list;
+                      return Container(
+                        color: index.isEven ? Colors.black12 : Colors.white,
+                        padding: const EdgeInsets.all(10),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            SizedBox(
+                                width: MediaQuery.of(context).size.width * 0.20,
+                                child: Text(
+                                  ld[index].word,
+                                  style: const TextStyle(
+                                    color: Colors.black,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                )),
+                            SizedBox(
+                              width: MediaQuery.of(context).size.width * 0.70,
                               child: Text(
-                                ld[index].word,
-                                style: const TextStyle(
-                                  color: Colors.black,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              )),
-                          SizedBox(
-                            width: MediaQuery.of(context).size.width * 0.70,
-                            child: Text(
-                              ld[index].definition,
-                            ),
-                          )
-                        ],
-                      ),
-                    );
-                  },
-                  separatorBuilder: (BuildContext context, int index) {
-                    return const SizedBox(
-                      height: 10,
-                    );
-                  },
-                ),
-              ),
-            ],
-          );
-        },
+                                ld[index].definition,
+                              ),
+                            )
+                          ],
+                        ),
+                      );
+                    },
+                    separatorBuilder: (BuildContext context, int index) {
+                      return const SizedBox(
+                        height: 10,
+                      );
+                    },
+                  );
+                }
+                return const SizedBox();
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
