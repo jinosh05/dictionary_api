60bcbd38f108f64136ddcb44cacb1a679f559aa7
Jinosh05 <jinoshprabhuraj5@gmail.com>
2023-09-02 23:16:08 +0530

    Applied New Lint rules

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
-import 'dart:convert';
+import "dart:convert";
+import "dart:developer";
 
-import 'package:dictionary_api/models/dictionary_model.dart';
-import 'package:flutter/material.dart';
-import 'package:http/http.dart';
-import 'package:url_strategy/url_strategy.dart';
+import "package:dictionary_api/models/dictionary_model.dart";
+import "package:flutter/material.dart";
+import "package:http/http.dart";
+import "package:url_strategy/url_strategy.dart";
 
 void main() {
   setPathUrlStrategy();
@@ -11,7 +12,7 @@ void main() {
 }
 
 class HomeScreen extends StatefulWidget {
-  const HomeScreen({Key? key}) : super(key: key);
+  const HomeScreen({super.key});
 
   @override
   State<HomeScreen> createState() => _HomeScreenState();
@@ -22,52 +23,51 @@ class _HomeScreenState extends State<HomeScreen> {
   final TextEditingController controller = TextEditingController();
 
   Future<DictionaryModel?> fetchApi(String value) async {
-    Response response = await get(
-        Uri.parse('https://api.urbandictionary.com/v0/define?term=$value'));
+    final Response response = await get(
+      Uri.parse("https://api.urbandictionary.com/v0/define?term=$value"),
+    );
 
     try {
       debugPrint(response.body);
       return model = DictionaryModel.fromJson(jsonDecode(response.body));
-    } catch (e) {
+    } on Exception catch (e) {
+      log(e.toString());
       return null;
     }
   }
 
-  void autoCompleteSearch(String value) async {
-    var result = await fetchApi(value);
+  Future<void> autoCompleteSearch(String value) async {
+    final DictionaryModel? result = await fetchApi(value);
     setState(() {
       model = result;
     });
   }
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Dictionary API'),
-      ),
-      body: Column(
-        children: [
-          _searchBar(context),
-          _resultContents(),
-        ],
-      ),
-    );
-  }
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(
+          title: const Text("Dictionary API"),
+        ),
+        body: Column(
+          children: <Widget>[
+            _searchBar(context),
+            _resultContents(),
+          ],
+        ),
+      );
 
-  Expanded _resultContents() {
-    return Expanded(
-      child: ListView.separated(
-        itemCount: model == null ? 0 : model!.list!.length,
-        itemBuilder: (BuildContext context, int index) {
-          List<ListData> ld = model!.list!;
-          return Container(
-            color: index.isEven ? Colors.black12 : Colors.white,
-            padding: const EdgeInsets.all(10),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                SizedBox(
+  Expanded _resultContents() => Expanded(
+        child: ListView.separated(
+          itemCount: model == null ? 0 : model!.list!.length,
+          itemBuilder: (BuildContext context, int index) {
+            final List<ListData> ld = model!.list!;
+            return Container(
+              color: index.isEven ? Colors.black12 : Colors.white,
+              padding: const EdgeInsets.all(10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  SizedBox(
                     width: MediaQuery.of(context).size.width * 0.20,
                     child: Text(
                       ld[index].word ?? "",
@@ -75,51 +75,49 @@ class _HomeScreenState extends State<HomeScreen> {
                         color: Colors.black,
                         fontWeight: FontWeight.bold,
                       ),
-                    )),
-                SizedBox(
-                  width: MediaQuery.of(context).size.width * 0.70,
-                  child: Text(
-                    ld[index].definition ?? "",
+                    ),
                   ),
-                )
-              ],
-            ),
-          );
-        },
-        separatorBuilder: (BuildContext context, int index) {
-          return const SizedBox(
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width * 0.70,
+                    child: Text(
+                      ld[index].definition ?? "",
+                    ),
+                  ),
+                ],
+              ),
+            );
+          },
+          separatorBuilder: (BuildContext context, int index) => const SizedBox(
             height: 10,
-          );
-        },
-      ),
-    );
-  }
+          ),
+        ),
+      );
 
-  Container _searchBar(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.symmetric(vertical: 10),
-      width: MediaQuery.of(context).size.width * 0.9,
-      child: TextField(
-        controller: controller,
-        decoration: InputDecoration(
+  Container _searchBar(BuildContext context) => Container(
+        margin: const EdgeInsets.symmetric(vertical: 10),
+        width: MediaQuery.of(context).size.width * 0.9,
+        child: TextField(
+          controller: controller,
+          decoration: InputDecoration(
             border: const OutlineInputBorder(),
             suffixIcon: IconButton(
-                onPressed: () {
-                  controller.clear();
-                  autoCompleteSearch(" ");
-                },
-                icon: const Icon(
-                  Icons.clear,
-                  size: 25,
-                ))),
-        onChanged: ((value) {
-          if (value.isNotEmpty) {
-            autoCompleteSearch(controller.text);
-          } else {
-            model = null;
-          }
-        }),
-      ),
-    );
-  }
+              onPressed: () async {
+                controller.clear();
+                await autoCompleteSearch(" ");
+              },
+              icon: const Icon(
+                Icons.clear,
+                size: 25,
+              ),
+            ),
+          ),
+          onChanged: (String value) async {
+            if (value.isNotEmpty) {
+              await autoCompleteSearch(controller.text);
+            } else {
+              model = null;
+            }
+          },
+        ),
+      );
 }
--- lib/models/dictionary_model.dart
+++ lib/models/dictionary_model.dart
@@ -1,28 +1,55 @@
 class DictionaryModel {
-  List<ListData>? list;
-
   DictionaryModel({this.list});
 
   DictionaryModel.fromJson(Map<String, dynamic> json) {
     list = <ListData>[];
-    if (json['list'] != null) {
-      List tempList = json['list'];
-      for (var v in tempList) {
+    if (json["list"] != null) {
+      final List<Map<String, dynamic>> tempList =
+          json["list"] as List<Map<String, dynamic>>;
+      for (final Map<String, dynamic> v in tempList) {
         list!.add(ListData.fromJson(v));
       }
     }
   }
+  List<ListData>? list;
 
   Map<String, dynamic> toJson() {
     final Map<String, dynamic> data = <String, dynamic>{};
     if (list != null) {
-      data['list'] = list!.map((v) => v.toJson()).toList();
+      data["list"] = list!.map((ListData v) => v.toJson()).toList();
     }
     return data;
   }
 }
 
 class ListData {
+  ListData({
+    this.definition,
+    this.permalink,
+    this.thumbsUp,
+    this.soundUrls,
+    this.author,
+    this.word,
+    this.defid,
+    this.currentVote,
+    this.writtenOn,
+    this.example,
+    this.thumbsDown,
+  });
+
+  ListData.fromJson(Map<String, dynamic> json) {
+    definition = json["definition"];
+    permalink = json["permalink"];
+    thumbsUp = json["thumbs_up"];
+    soundUrls = json["sound_urls"];
+    author = json["author"];
+    word = json["word"];
+    defid = json["defid"];
+    currentVote = json["current_vote"];
+    writtenOn = json["written_on"];
+    example = json["example"];
+    thumbsDown = json["thumbs_down"];
+  }
   String? definition;
   String? permalink;
   int? thumbsUp;
@@ -35,46 +62,19 @@ class ListData {
   String? example;
   int? thumbsDown;
 
-  ListData(
-      {this.definition,
-      this.permalink,
-      this.thumbsUp,
-      this.soundUrls,
-      this.author,
-      this.word,
-      this.defid,
-      this.currentVote,
-      this.writtenOn,
-      this.example,
-      this.thumbsDown});
-
-  ListData.fromJson(Map<String, dynamic> json) {
-    definition = json['definition'];
-    permalink = json['permalink'];
-    thumbsUp = json['thumbs_up'];
-    soundUrls = json['sound_urls'];
-    author = json['author'];
-    word = json['word'];
-    defid = json['defid'];
-    currentVote = json['current_vote'];
-    writtenOn = json['written_on'];
-    example = json['example'];
-    thumbsDown = json['thumbs_down'];
-  }
-
   Map<String, dynamic> toJson() {
     final Map<String, dynamic> data = <String, dynamic>{};
-    data['definition'] = definition;
-    data['permalink'] = permalink;
-    data['thumbs_up'] = thumbsUp;
-    data['sound_urls'] = soundUrls;
-    data['author'] = author;
-    data['word'] = word;
-    data['defid'] = defid;
-    data['current_vote'] = currentVote;
-    data['written_on'] = writtenOn;
-    data['example'] = example;
-    data['thumbs_down'] = thumbsDown;
+    data["definition"] = definition;
+    data["permalink"] = permalink;
+    data["thumbs_up"] = thumbsUp;
+    data["sound_urls"] = soundUrls;
+    data["author"] = author;
+    data["word"] = word;
+    data["defid"] = defid;
+    data["current_vote"] = currentVote;
+    data["written_on"] = writtenOn;
+    data["example"] = example;
+    data["thumbs_down"] = thumbsDown;
     return data;
   }
 }
