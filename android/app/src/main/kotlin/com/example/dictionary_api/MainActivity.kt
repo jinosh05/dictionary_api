29fa4a7bf0ba1826fc92b3c162c1647298baa107
Jinosh05 <jinoshprabhuraj5@gmail.com>
2024-07-27 00:10:01 +0530

    Android package updated

--- android/app/src/main/kotlin/com/example/dictionary_api/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.dictionary_api
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ android/app/src/main/kotlin/com/example/dictionary_api_bloc/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.dictionary_api_bloc
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
